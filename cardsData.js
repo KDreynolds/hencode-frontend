d3ad0b31eefa8ff04d70b1d2ce31eba54d17d8f0
Kyle Reynolds <kylereynoldsdev@gmail.com>
2023-08-04 16:13:30 -0600

    Add files via upload

---
+++ cardsData.js
@@ -0,0 +1,37 @@
+let cardsData = [{
+	id: 1,
+	image: './images/app-page/pexels-negative-space-97077-2-1673936-scaled.jpg',
+	avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+	user: {
+	  title: 'Base64'
+	},
+	content: 'Encode and decode base64 text'
+  },
+  {
+	id: 2,
+	image: './images/app-page/shutterstock_1165235053.jpg',
+	avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+	user: {
+	  title: 'Hexidecimal'
+	},
+	content: 'Encode and decode hexidecimal text'
+  },
+  {
+	id: 3,
+	image: './images/app-page/Encoding.webp',
+	avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+	user: {
+	  title: 'URL'
+	},
+	content: 'Encode and decode URL text'
+  },
+  {
+	id: 4,
+	image: './images/app-page/encode1.jpg',
+	avatar: 'https://avatars.dicebear.com/api/initials/john%20doe.svg',
+	user: {
+	  title: 'Binary'
+	},
+	content: 'Encode and decode binary text'
+  },
+  ]
